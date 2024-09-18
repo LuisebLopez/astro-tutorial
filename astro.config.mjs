0c6c91adff3884a2ccab62b9606d0feb026f36d5
Luis Lopez <luise.lopez.erazo@gmail.com>
2024-09-18 13:25:06 -0600

    I finished the astro tutorial yey

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrotutorialebleb.netlify.app"
+  site: "https://astrotutorialebleb.netlify.app",
+  integrations: [preact()]
 });
